ff043d849e3b1425d1b9f08949be5627281cb101
cooldev225 <goldsatr22501@gmail.com>
2023-03-17 05:28:06 -0600

    make migration

---
+++ migrations/Version20230317110757.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230317110757 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE fruit (id INT AUTO_INCREMENT NOT NULL, genus VARCHAR(255) DEFAULT NULL, name VARCHAR(255) NOT NULL, fruit_id INT NOT NULL, family VARCHAR(255) NOT NULL, fruit_order VARCHAR(255) DEFAULT NULL, carbohydrates DOUBLE PRECISION DEFAULT NULL, protein DOUBLE PRECISION DEFAULT NULL, fat DOUBLE PRECISION DEFAULT NULL, calories DOUBLE PRECISION DEFAULT NULL, sugar DOUBLE PRECISION DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE fruit');
+    }
+}
